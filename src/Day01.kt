eefb6600c160034b804c2f541f14c6126314061d
Stephan Sundermann <stephansundermann@gmail.com>
2024-12-01 12:24:25 +0100

    Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,33 @@
+import kotlin.math.absoluteValue
+
 fun main() {
+    fun String.parse() = this.split("   ")
+        .map { it.toInt() }.let { it[0] to it[1] }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val (fst, snd) = input
+            .map { it.parse() }
+            .unzip()
+
+        return fst.sorted()
+            .zip(snd.sorted())
+            .sumOf { (a, b) -> (a - b).absoluteValue }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val (fst, snd) = input
+            .map { it.parse() }
+            .unzip()
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        val freq = snd.groupingBy { it }.eachCount()
+
+        return fst.sumOf { it * (freq[it] ?: 0) }
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
